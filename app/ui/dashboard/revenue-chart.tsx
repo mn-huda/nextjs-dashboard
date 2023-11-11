de6531b1ea3818b241a7df37eab984d1088a7c44
Md. Nazmul Huda <me@nazmul-huda.com>
2023-11-11 11:18:24 +0600

    set up before db connection

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,7 @@
-import { generateYAxis } from '@/app/lib/utils';
-import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import { generateYAxis } from '@/app/lib/utils'
+import { CalendarIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
+import { Revenue } from '@/app/lib/definitions'
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -10,11 +10,11 @@ import { Revenue } from '@/app/lib/definitions';
 // https://airbnb.io/visx/
 
 export default async function RevenueChart({
-  revenue,
+  revenue
 }: {
-  revenue: Revenue[];
+  revenue: Revenue[]
 }) {
-  const chartHeight = 350;
+  const chartHeight = 350
   // NOTE: comment in this code when you get to this point in the course
 
   // const { yAxisLabels, topLabel } = generateYAxis(revenue);
@@ -24,8 +24,10 @@ export default async function RevenueChart({
   // }
 
   return (
-    <div className="w-full md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+    <div className='w-full md:col-span-4'>
+      <h2
+        className={`${lusitana.className} mb-4 text-xl md:text-2xl`}
+      >
         Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
@@ -61,5 +63,5 @@ export default async function RevenueChart({
         </div>
       </div> */}
     </div>
-  );
+  )
 }
